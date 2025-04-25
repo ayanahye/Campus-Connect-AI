d1adb3f35ccfac10ee4c9657fd7a1b1b531fbc95
Poorvi <poorvi.b26@gmail.com>
2025-04-25 11:37:38 +0530

    cleaned up repo

--- cost-of-living-data/get-unique-types.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("original_2021_bc_cost_by_household_type.csv")
-unique_values = df["Household expenditures, summary-level categories"].unique()
-for value in unique_values:
-    print(value)
-print(len(unique_values))
--- personalization-food-data/drop_du.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("qa_pairs_food.csv")
-
-df=df.drop_duplicates()
-
-df.to_csv("qa_pairs_food.csv", index=False)
