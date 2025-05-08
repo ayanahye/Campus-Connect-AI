ec593cb268b586d757e38692790adb147dd900ca
ayana <ayanahye2@gmail.com>
2025-05-08 01:00:18 -0700

    small notes reminder to add

--- eval/get_judge_evals_retrv/calc-unseen.py
+++ eval/get_judge_evals_retrv/calc-unseen.py
@@ -28,3 +28,11 @@ MAP: 0.361
 '''
 
 # take w grain of salt, i didnt have much time to actually go thru the models outputs so not sure how good the evals are or even if the metrics should be used, in all maybe a better embedding model is needed but again we can highlight the tradeoff between time and accuracy
+
+'''
+actually highlights 2 things (given that my code is correct: please verify)
+- 1) potentially we are missing documents in relevant areas (here is another key, we can always add more data and while testing and searching online for relevant faq from international students we realized we are missing some important areas (these results also support that) thus highlighting the importance of developing alongside real users i.e. international students but due to time constraints we leave for future iterations on this)
+- 2) potentially that the generator can do well enough given its high scores on generation on unseen data even with docs that arent so helpful. Also, that the generator model was able to ignore info when it was not relveant
+
+we can report this in presentation
+'''
